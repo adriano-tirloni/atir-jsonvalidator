2a2140e7311e0ef4ce877d25b670489176f24f84
Adriano Tirloni <tirloni.adriano@gmail.com>
2024-01-22 12:24:13 -0300

    added i18n

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { compileSchema, getValidator, createValidator } from './jsonValidator.js';
-import * as i18n from "./i18n_functions/index.js"
+import * as i18nFunctions from "./i18n_functions/index.js"
+const i18n = i18nFunctions
 
 export { compileSchema, getValidator, createValidator, i18n }
