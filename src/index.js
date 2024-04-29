94f091e5503fe38c24ece4d4638757f82be54628
Adriano Tirloni <tirloni.adriano@gmail.com>
2024-04-29 09:58:43 -0300

    version teste

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { compileSchema, getValidator, createValidator } from './jsonValidator.js';
 export * as i18n from "./i18n_functions/index.js"
 export { compileSchema, getValidator, createValidator }
+//
