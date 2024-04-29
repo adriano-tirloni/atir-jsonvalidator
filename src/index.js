27d5b7d33b566ba64af7bfd5a1a92c35fc7f8ac5
Adriano Tirloni <tirloni.adriano@gmail.com>
2024-04-29 11:58:52 -0300

    version bump

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import { compileSchema, getValidator, createValidator } from './jsonValidator.js';
 export * as i18n from "./i18n_functions/index.js"
 export { compileSchema, getValidator, createValidator }
-//19
+//20
